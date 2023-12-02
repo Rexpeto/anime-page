cbd9e9b1ce3ab2f62259f2bcb55b28860f13d58c
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-12-02 19:23:40 -0400

    footer component

---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="flex flex-wrap justify-between items-center gap-2 py-4 px-4 sm:px-16 bg-gray-800/40">
+      <p className="text-white font-semibold">
+        Copyright © 2023 - All right reserved
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
