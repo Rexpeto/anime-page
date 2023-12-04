df0dbaf6e8a23e6355ad50c09835d4b6577b1b8e
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-12-04 12:20:53 -0400

    update barrel

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as Hero } from "./hero";
 export { default as Footer } from "./footer";
 export { default as Card } from "./card";
 export { default as GetIcons } from "./getIcons";
+export { default as LoadMore } from "./loadMore";
