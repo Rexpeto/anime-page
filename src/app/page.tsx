27766d449de6ba6db114a16d94ea779e53b547aa
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-12-05 01:04:10 -0400

    call anime service

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import { Card, LoadMore } from "@/components";
-import { AnimeResult, animeData } from "@/models";
+import { AnimeResult } from "@/models";
+import { fetchAnime } from "@/services";
 
-const Home = () => {
+const Home = async () => {
+  const data = await fetchAnime(1);
+
   return (
     <main className="sm:p-16 py-16 px-8 flex flex-col gap-10">
       <h2 className="text-3xl text-white font-bold">Explore Anime</h2>
 
-      <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
-        {animeData.map((item: AnimeResult) => (
+      <section className="grid lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+        {data.map((item: AnimeResult) => (
           <Card key={item.id} anime={item} />
         ))}
       </section>
