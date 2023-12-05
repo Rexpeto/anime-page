1c78d36d7a8390f594b57f00c404fd584c27bc13
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-12-05 01:01:27 -0400

    use environment

--- src/services/anime.service.ts
+++ src/services/anime.service.ts
@@ -2,7 +2,7 @@
 
 export const fetchAnime = async (page: number) => {
   const response = await fetch(
-    `https://shikimori.one/api/animes?page=${page}&limit=8&order=popularity`,
+    `${process.env.URL_API}/api/animes?page=${page}&limit=10&order=popularity`,
   );
 
   const data = await response.json();
